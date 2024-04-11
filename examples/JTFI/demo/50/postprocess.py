83b47ddbf8e54a2847caa8db41256fddbd66cba5
Gary Hu <thu@anl.gov>
2024-04-11 10:25:49 -0500

    update

--- examples/JTFI/demo/50/postprocess.py
+++ examples/JTFI/demo/50/postprocess.py
@@ -7,7 +7,7 @@ result = pd.read_csv("CC_charging_out.csv")
 
 fig, ax = plt.subplots()
 ax.plot(result["C"], result["V"])
-ax.set(xlabel="C", ylabel="V", ylim=[3, 5])
+ax.set(xlabel="C", ylabel="V", ylim=[3, 4.5])
 fig.tight_layout()
 fig.savefig("CV.png")
 
@@ -18,6 +18,6 @@ dQ_dV = savgol_filter(np.diff(result["C"]), 50, 1, mode="nearest") / savgol_filt
 )
 fig, ax = plt.subplots()
 ax.plot(result["V"][1:], dQ_dV)
-ax.set(xlabel="V", ylabel="dQ/dV", xlim=[3, 5], ylim=[-0.2, 0.2])
+ax.set(xlabel="V", ylabel="dQ/dV", xlim=[3, 4.5], ylim=[-0.2, 0.2])
 fig.tight_layout()
 fig.savefig("dQ_dV.png")
